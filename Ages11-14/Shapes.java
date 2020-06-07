e1db30be493e7881c82389fe14ce1d924b13d286
Bob Baker <007dsi@gmail.com>
2020-06-06 23:15:45 -0700

    Add files via upload

--- Ages11-14/Shapes.java
+++ Ages11-14/Shapes.java
@@ -2,20 +2,27 @@ import java.util.Scanner;       // Import the Input-Output Library
 public class Shapes{
 
      public static void main(String []args){
+
+        // Start the input scanner
         Scanner scanner = new Scanner( System.in );
         
-         
         System.out.println("Area of a Rectangle:\n");
+
+        // Get the dimensions of the rectangle
         System.out.print("Enter the width of the rectangle: ");
         double recWidth = scanner.nextDouble();
         
         System.out.print("Enter the height of the rectangle: ");
         double recHeight = scanner.nextDouble();
         
+        // Calculate the Area (A=W*H)
         double recArea = recWidth * recHeight;
         
+        // Ouput the Area
         System.out.println("\nThe Area of the rectangle is: " + recArea);
 
+        
+        // Close in input scanner
         scanner.close();
      }
 }
