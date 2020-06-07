382e0ef4eb7366ae1caddd0935808afcde159afe
Bob Baker <rbaker2614@gmail.com>
2020-06-06 22:57:03 -0700

    Update HelloWorld.cpp

--- Ages11-14/HelloWorld.cpp
+++ Ages11-14/HelloWorld.cpp
@@ -1,1 +1,9 @@
-d
+#include <iostream> // Include the Input-Output Library
+
+int main(int argc, char *argv[])
+{
+    char myString[] = "Hello World!";     // Create the HelloWorld string
+    std::cout << myString << std::endl;   // Output the string to the console
+    return 0;                             // Exit the program
+}
+  
