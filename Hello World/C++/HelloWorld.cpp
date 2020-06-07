4cb7a07883a6d7d71164d09ed68befa26366e67e
Bob Baker <rbaker2614@gmail.com>
2020-06-06 22:57:26 -0700

    Update HelloWorld.cpp

--- Hello World/C++/HelloWorld.cpp
+++ Hello World/C++/HelloWorld.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream> // Include the Input-Output Library
 
 int main(int argc, char *argv[])
 {
-    char myString[] = "Hello World!";
-    std::cout << myString << std::endl;
-    return 0;
+    char myString[] = "Hello World!";     // Create the HelloWorld string
+    std::cout << myString << std::endl;   // Output the string to the console
+    return 0;                             // Exit the program
 }
   
